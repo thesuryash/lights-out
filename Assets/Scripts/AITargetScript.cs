8c979ed5f17765bdd6b38bb0e14b56360dbfaa0b
FisherG05 <fisher.griesel@outlook.com>
2025-12-02 15:00:45 -0500

    added werewolf movement

---
+++ Assets/Scripts/AITargetScript.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AITargetScript : MonoBehaviour
+{
+    public Transform Target;
+    public float AttackDistance;
+    public float TargetingDistance;
+
+    private NavMeshAgent m_Agent;
+    private float m_Distance;
+
+
+    void Start() {
+        m_Agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update() {
+        m_Distance = Vector3.Distance(m_Agent.transform.position, Target.position);
+        if (m_Distance < AttackDistance && m_Distance > TargetingDistance) {
+            m_Agent.isStopped = true;
+        }
+        else {
+            m_Agent.isStopped = false;
+            m_Agent.destination = Target.position;
+        }
+    }
+}
